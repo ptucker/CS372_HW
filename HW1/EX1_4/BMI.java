c8aa043d71d845581552028c5ee85477a4055e53
Peter Tucker <ptucker@whitworth.edu>
2019-01-09 09:52:01 -0800

    feedback

--- HW1/EX1_4/BMI.java
+++ HW1/EX1_4/BMI.java
@@ -4,6 +4,8 @@ public class BMI{
     public static void main(String[] args){
         System.out.println("Welcome to the BMI Calculator! So I can calculate, may I have your weight in pounds?");
         Scanner scnr = new Scanner(System.in);
+        //PT -- name these, as you did for PI
+        // final double POUND_PER_KG = 2.205;
         double weight = scnr.nextDouble() / 2.205;
         System.out.println("And may I also ask you your height in inches?");
         double height = scnr.nextDouble() / 39.37;
